dac001831627cf3cafb26dc63d7081bb4cdd41c4
Sourav Datta <sdatta@planview.com>
2021-08-08 20:09:32 +0530

    Create MessagePay.sol
    Send messages to different accounts and view them. Also if your message contains '$' then send that much unit of value to the account as well.

---
+++ MessagePay.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >0.5.0 <=0.9.0;
+
+contract Messages {
+    mapping(address => mapping(address => string[])) addressMap;
+    mapping(address => int) balance;
+    
+    function putMessage(address _to, string memory _message) public {
+        bytes memory bytesString = bytes(_message);
+        int sumDollars = 0;
+        for (uint i = 0; i < bytesString.length; i++) {
+            if (bytesString[i] == "$") {
+                sumDollars += 1;
+            }
+        }
+        
+        if (sumDollars > 0) {
+            int myBalance = balance[msg.sender];
+            require(myBalance >= sumDollars);
+            
+            balance[_to] += sumDollars;
+            balance[msg.sender] -= sumDollars;
+        }
+        
+        addressMap[_to][msg.sender].push(_message);
+        
+    }
+    
+    function getMessage(address _from) public view returns (string[] memory) {
+        return addressMap[msg.sender][_from];
+    }
+    
+    function getBalance() public view returns (int) {
+        return balance[msg.sender];
+    }
+    
+    function setBalance(int sum) public {
+        balance[msg.sender] = sum;
+    }
+}
