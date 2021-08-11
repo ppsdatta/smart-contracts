54b05891745939372122d827bf59fdb0a833cd04
Sourav Datta <sdatta@planview.com>
2021-08-11 10:45:24 +0530

    Update the name of the contract to match file name

--- MessagePay.sol
+++ MessagePay.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >0.5.0 <=0.9.0;
 
-contract Messages {
+contract MessagePay {
     address public centralBank;
     mapping(address => mapping(address => string[])) addressMap;
     mapping(address => int) balance;
